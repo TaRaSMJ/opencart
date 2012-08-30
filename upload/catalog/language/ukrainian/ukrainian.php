0e0ceb81b91119d38da10251ba84b5fee2a96139
Eugene Kuligin <k.evgen@meta.ua>
2012-08-30 17:03:16 +0300

    add ukrainian language (for 1.5.3)

---
+++ upload/catalog/language/ukrainian/ukrainian.php
@@ -0,0 +1,72 @@
+<?php
+/***********************************************************************************************************************
+
+Version 1.5.3
+Created 17:23 16.07.2012 UTF-8
+Copyright (C) 2010 Ukrainian Localization by OpenCart Ukrainian Team (http://opencart.ua). All rights reserved.
+
+Translators:
+ Eugene Kuligin (k.evgen@meta.ua)
+ and others.
+
+Responsible for Translation:
+ Eugene Kuligin (admin@opencart.ua)
+
+License http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+Ask a question http://forum.opencart.ua
+
+************************************************************************************************************************/
+
+$_['code'] = 'uk'; #31817
+$_['direction'] = 'ltr'; #31824
+$_['date_format_short'] = 'd.m.Y'; #31818
+$_['date_format_long'] = 'd.m.Y'; #31826
+$_['time_format'] = 'H:i:s'; #31819
+$_['decimal_point'] = '.'; #31790
+$_['thousand_point'] = ','; #31791
+$_['text_home'] = 'Головна'; #31792
+$_['text_yes'] = 'Так'; #31793
+$_['text_no'] = 'Ні'; #31794
+$_['text_none'] = '--- Немає ---'; #31795
+$_['text_select'] = '--- Оберіть ---'; #31796
+$_['text_all_zones'] = 'Усі зони'; #31797
+$_['text_pagination'] = 'Показано {start} по {end} із {total} (всього сторінок: {pages})'; #31825
+$_['text_separator'] = ' &gt; '; #31798
+$_['button_add_address'] = 'Додати адресу'; #31801
+$_['button_back'] = 'Назад'; #31800
+$_['button_continue'] = 'Продовжити'; #31799
+$_['button_cart'] = 'До кошика'; #33505
+$_['button_compare'] = 'Порівняти'; #33506
+$_['button_wishlist'] = 'В улюблені'; #33507
+$_['button_checkout'] = 'Оформити'; #31811
+$_['button_confirm'] = 'Підтвердіть замовлення'; #31812
+$_['button_coupon'] = 'Використати'; #31815
+$_['button_delete'] = 'Видалити'; #31805
+$_['button_download'] = 'Завантаження'; #35143
+$_['button_edit'] = 'Змінити'; #31804
+$_['button_new_address'] = 'Нова адреса'; #31802
+$_['button_change_address'] = 'Оберіть адресу'; #31803
+$_['button_reviews'] = 'Відгуки'; #31806
+$_['button_write'] = 'Редагувати відгуки'; #31807
+$_['button_login'] = 'Увійти'; #31808
+$_['button_update'] = 'Оновити'; #31809
+$_['button_remove'] = 'Видалити'; #33508
+$_['button_reorder'] = 'Повторне замовлення'; #35144
+$_['button_return'] = 'Повернення'; #35145
+$_['button_shopping'] = 'Продовжити покупки'; #31810
+$_['button_search'] = 'Пошук'; #31814
+$_['button_shipping'] = 'Підтвердити доставку'; #33509
+$_['button_guest'] = 'Гостьове замовлення'; #31816
+$_['button_view'] = 'Перегляд'; #31813
+$_['button_voucher'] = 'Застосувати подарунковий сертифікат'; #34564
+$_['button_upload'] = 'Завантажити файл'; #33510
+$_['button_reward'] = 'Використати бали'; #34487
+$_['button_quote'] = 'Отримати ціни'; #34488
+$_['error_upload_1'] = 'Файл перевищує розмір, встановлений директивою upload_max_filesize у файлі php.ini!'; #33511
+$_['error_upload_2'] = 'Файл перевищує розмір, встановлений параметром форми MAX_FILE_SIZE!'; #33512
+$_['error_upload_3'] = 'Файл завантажено тільки частково!'; #33513
+$_['error_upload_4'] = 'Немає файлу для завантаження!'; #33514
+$_['error_upload_6'] = 'Не знайдено тимчасову теку!'; #33515
+$_['error_upload_7'] = 'Невдалий запис файлу на диск!'; #33516
+$_['error_upload_8'] = 'Завантаження зупинене у зв’язку з розширенням!'; #33517
+$_['error_upload_999'] = 'Невідомий код помилки!'; #33518
